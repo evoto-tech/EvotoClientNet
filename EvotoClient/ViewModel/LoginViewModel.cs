a028070d1aeee61eb5187a1d858e1b3bca20b8d7
Michael <michaelfiford@gmail.com>
2017-01-14 13:17:39 +0000

    Handle password securely

--- EvotoClient/ViewModel/LoginViewModel.cs
+++ EvotoClient/ViewModel/LoginViewModel.cs
@@ -1,27 +1,37 @@
-﻿using System.Threading;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Api.Clients;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using Microsoft.Practices.ServiceLocation;
 
 namespace EvotoClient.ViewModel
 {
+    public interface IHavePassword
+    {
+        SecureString Password { get; }
+    }
+
     public class LoginViewModel : ViewModelBase
     {
+        private readonly LoginClient _loginClient;
         private bool _loading;
         private MainViewModel _mainVm;
         private string _username;
 
         public LoginViewModel()
         {
-            LoginCommand = new RelayCommand(Login, CanLogin);
+            _loginClient = new LoginClient();
+            LoginCommand = new RelayCommand<object>(Login, CanLogin);
             CanSubmit = CanExecuteChanged;
         }
 
         private MainViewModel MainVm => _mainVm ?? (_mainVm = ServiceLocator.Current.GetInstance<MainViewModel>());
 
-        public RelayCommand LoginCommand { get; }
+        public RelayCommand<object> LoginCommand { get; }
 
         public bool Loading
         {
@@ -41,18 +51,19 @@ namespace EvotoClient.ViewModel
 
         public KeyEventHandler CanSubmit { get; }
 
-        private void Login()
+        private void Login(object parameter)
         {
+            var passwordContainer = parameter as IHavePassword;
+            if (passwordContainer == null)
+                return;
+
             Loading = true;
 
-            Task.Factory.StartNew(() =>
-            {
-                Thread.Sleep(5000);
-                MainVm.ChangeView(EvotoView.Home);
-            });
+            Task.Factory.StartNew(
+                async () => { await _loginClient.Login(Username, ConvertToUnsecureString(passwordContainer.Password)); });
         }
 
-        private bool CanLogin()
+        private bool CanLogin(object data)
         {
             return (Username?.Length > 0) && !Loading;
         }
@@ -60,6 +71,22 @@ namespace EvotoClient.ViewModel
         private void CanExecuteChanged(object sender, KeyEventArgs e)
         {
             LoginCommand.RaiseCanExecuteChanged();
+        }
+
+        public string ConvertToUnsecureString(SecureString securePassword)
+        {
+            if (securePassword == null) return string.Empty;
+
+            var unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
         }
     }
 }
