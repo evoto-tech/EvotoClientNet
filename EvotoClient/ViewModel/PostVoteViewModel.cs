a1d3eccfa0f2be000b5700e581abf1659c8a1d1e
Michael <michaelfiford@gmail.com>
2017-04-14 16:47:46 +0100

    update post vote view and give the user a lot more info. Copy and paste support

--- EvotoClient/ViewModel/PostVoteViewModel.cs
+++ EvotoClient/ViewModel/PostVoteViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using GalaSoft.MvvmLight.Command;
 using Models;
 
@@ -11,33 +12,69 @@ namespace EvotoClient.ViewModel
         public PostVoteViewModel()
         {
             ProceedCommand = new RelayCommand(DoProceed, CanProceed);
+            HomeCommand = new RelayCommand(DoHome);
+            CopyCommand = new RelayCommand(DoCopy);
+        }
+
+        public void Voted(BlockchainDetails blockchain, string words)
+        {
+            CopyStatus = "";
+            MagicWords = words;
+            _blockchain = blockchain;
+
+            Encrypted = blockchain.ShouldEncryptResults;
+            ProceedCommand.RaiseCanExecuteChanged();
         }
 
         #region Commands
 
         public RelayCommand ProceedCommand { get; }
+        public RelayCommand HomeCommand { get; }
+        public RelayCommand CopyCommand { get; }
 
         #endregion
 
-        public void Voted(BlockchainDetails blockchain, string words)
+        #region Properties
+
+        public bool _encrypted;
+
+        public bool Encrypted
         {
-            MagicWords = words;
-            _blockchain = blockchain;
-            RaisePropertyChanged(nameof(ResultsEnabled));
+            get { return _encrypted; }
+            set { Set(ref _encrypted, value); }
         }
 
-        #region Properties
+        public string EncryptedText
+        {
+            get
+            {
+                var remaining = GetTimeRemaining();
+                var time = _blockchain.ExpiryDate.ToShortTimeString();
+                var date = _blockchain.ExpiryDate.ToShortDateString();
+
+                return
+                    "This blockchain is encrypted. \n" +
+                    "This means that the results are not accessible until after the vote has finished.\n" +
+                    $"Please check back in {remaining} ({time} {date}).";
+            }
+        }
+
 
         private string _magicWords;
 
         public string MagicWords
         {
-            get { return $"Magic Words: " + _magicWords; }
+            get { return _magicWords; }
             set { Set(ref _magicWords, value); }
         }
 
-        public bool ResultsEnabled
-            => string.IsNullOrWhiteSpace(_blockchain.EncryptKey) || (_blockchain.ExpiryDate > DateTime.Now);
+        private string _copyStatus;
+
+        public string CopyStatus
+        {
+            get { return _copyStatus; }
+            set { Set(ref _copyStatus, value); }
+        }
 
         #endregion
 
@@ -52,7 +89,24 @@ namespace EvotoClient.ViewModel
 
         private bool CanProceed()
         {
-            return ResultsEnabled;
+            return !Encrypted;
+        }
+
+        private void DoHome()
+        {
+            MainVm.ChangeView(EvotoView.Home);
+        }
+
+        private void DoCopy()
+        {
+            Clipboard.SetText(MagicWords);
+            CopyStatus = "Copied!";
+        }
+
+        private string GetTimeRemaining()
+        {
+            var span = _blockchain.ExpiryDate - DateTime.Now;
+            return $"{span.Days} days, {span.Hours} hours, {span.Minutes} minutes";
         }
 
         #endregion
