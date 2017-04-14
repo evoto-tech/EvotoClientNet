e8373b834f0fe063cbf45442749041573346b1a9
Michael <michaelfiford@gmail.com>
2017-04-14 19:25:46 +0100

    Add datepicker for date fields

--- EvotoClient/ViewModel/CustomUserFieldViewModel.cs
+++ EvotoClient/ViewModel/CustomUserFieldViewModel.cs
@@ -1,10 +1,13 @@
-﻿using Models;
+﻿using System;
+using Models;
 
 namespace EvotoClient.ViewModel
 {
     public class CustomUserFieldViewModel : EvotoViewModelBase
     {
         private readonly CustomUserField _model;
+
+        private string _value;
 
         public CustomUserFieldViewModel(CustomUserField model)
         {
@@ -15,12 +18,31 @@ namespace EvotoClient.ViewModel
 
         public bool Required => _model.Required;
 
-        private string _value;
         public string Value
         {
             get { return _value; }
             set { Set(ref _value, value); }
         }
+
+        public DateTime? Date
+        {
+            get
+            {
+                try
+                {
+                    return DateTime.Parse(_value);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            set { Value = value?.ToString(); }
+        }
+
+        public bool ShowText => _model.Type != "Date";
+
+        public bool ShowDate => _model.Type == "Date";
 
         public CustomUserField GetModel()
         {
